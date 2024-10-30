344334c5ae325e5f804efcca31208f50b59d858a
haris <katerelosharry@gmail.com>
2024-10-30 19:10:17 +0200

    first commit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,22 +1,19 @@
 import UIKit
 import Flutter
-import AppCenter  // Base App Center framework
-import AppCenterAnalytics  // Analytics module
-import AppCenterCrashes  // Crashes module
-
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      // Initialize App Center with the manually set app secret and selected services
-      MSAppCenter.start("a68fac47fcd362df1f3432d7678e0d23c1645e58", withServices: [
-        MSAnalytics.self,  // Note the "MS" prefix for manually integrated SDKs
-        MSCrashes.self
+      AppCenter.start(withAppSecret: "a68fac47fcd362df1f3432d7678e0d23c1645e58", services:[
+        Analytics.self,
+        Crashes.self
       ])
-
-      GeneratedPluginRegistrant.register(with: self)
-      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }

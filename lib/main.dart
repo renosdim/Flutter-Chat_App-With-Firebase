6db925759956f6db562c3c9eb04c968b48a35f50
haris <katerelosharry@gmail.com>
2024-11-01 20:34:19 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ typedef AppBuilder = Future<Widget> Function();
 
 Future<void> bootstrap(AppBuilder builder) async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   //WidgetsBinding.instance.addObserver(AppLifecycleListener());
   //FirebaseDatabase.instance.setPersistenceEnabled(true);
 
